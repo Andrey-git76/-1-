8544dbe3f668ee93107e04237322a32efd8eab98
Andrey-git76 <astimo2006@gmail.com>
2024-12-26 18:21:01 +0300

    Добавлен новый файл

---
+++ Лаба 4.py
@@ -0,0 +1,14 @@
+##Задание №1
+from math import*
+a = input()
+print(sqrt(int(a)))
+from datetime import*
+print(datetime.now())
+##Задание №2
+import moda
+result = moda.sum_(6, 7)
+print(result)
+##Задание №3
+import moda
+res = 
+print('Результат работы функии:', res)
